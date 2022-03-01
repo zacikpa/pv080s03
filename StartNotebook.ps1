62b1590a7817a908095da8b3683d3533bb266456
Pavol Žáčik <zacik.pa@gmail.com>
2022-03-01 10:23:16 +0100

    Add the setup for B117 machines

---
+++ StartNotebook.ps1
@@ -0,0 +1,11 @@
+# This is a PowerShell script that will open up Jupyter notebook using
+# the single read-only virtual.
+#
+# This will only work in the room B117 on the faculty machines.
+# Do not run this elsewhere.
+
+# activate the virtual environment
+I:\pv080\seminars\venv\Scripts\activate.ps1
+
+# start the notebook
+jupyter notebook
